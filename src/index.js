80e1f365d667965aa5add7d76fe8d193caab5cfe
Ankur Chaudhary <Ankur@cloudthing.com>
2019-12-30 11:02:21 +0530

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router, Switch, NavLink } from 'react-router-dom'
+
 import './index.css';
+import Contact from './contact'
 import App from './App';
+import Home from './home'
+import NotFound from './notfound';
+
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/todo">ToDoApp</NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/todo" component={App} />
+                <Route path="/contact" component={Contact} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
 
 ReactDOM.render(
-    <App />, 
+    routing, 
     document.getElementById('root')
 );
 
