d4dabdda2a36325679eacbb08f2ce80e922a200e
Ankur Chaudhary <Ankur@cloudthing.com>
2019-12-30 10:21:36 +0530

    Updates

---
+++ precache-manifest.4623b4ab3cdf9839ca678d4dfe7324a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9e5ff7149f0adfaba1267b38889b6c",
+    "url": "/ankurchaudhary627/To-do-app/index.html"
+  },
+  {
+    "revision": "5f39dc6d6698494ea88b",
+    "url": "/ankurchaudhary627/To-do-app/static/css/main.5c286aa3.chunk.css"
+  },
+  {
+    "revision": "ee5d514afdec089f40ba",
+    "url": "/ankurchaudhary627/To-do-app/static/js/2.93f3605d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ankurchaudhary627/To-do-app/static/js/2.93f3605d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f39dc6d6698494ea88b",
+    "url": "/ankurchaudhary627/To-do-app/static/js/main.e9326fa6.chunk.js"
+  },
+  {
+    "revision": "0dc8ba950e6ddb71b195",
+    "url": "/ankurchaudhary627/To-do-app/static/js/runtime-main.795317ad.js"
+  }
+]);
