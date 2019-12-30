7948ad83cee74be8ca373514c360537568ec6f5a
Ankur Chaudhary <Ankur@cloudthing.com>
2019-12-30 11:43:49 +0530

    Updates

--- precache-manifest.c950064c22bdbb922d1398cdb599e085.js
+++ precache-manifest.c950064c22bdbb922d1398cdb599e085.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9dd441a58446ab153f42e11acaf2d778",
+    "revision": "f553e443d46f195ec864d4e6723bd7f3",
     "url": "/ankurchaudhary627/To-do-app/index.html"
   },
   {
-    "revision": "4c1a73ce6eea6ecd56bf",
+    "revision": "2344c3beae98e0ba0e63",
     "url": "/ankurchaudhary627/To-do-app/static/css/main.0ba043a4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ankurchaudhary627/To-do-app/static/js/2.d2654edd.chunk.js.LICENSE"
   },
   {
-    "revision": "4c1a73ce6eea6ecd56bf",
-    "url": "/ankurchaudhary627/To-do-app/static/js/main.460dd483.chunk.js"
+    "revision": "2344c3beae98e0ba0e63",
+    "url": "/ankurchaudhary627/To-do-app/static/js/main.8429645c.chunk.js"
   },
   {
     "revision": "0dc8ba950e6ddb71b195",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ankurchaudhary627/To-do-app/precache-manifest.c65282875dc54111c3cf5acae9d35ae6.js"
+  "/ankurchaudhary627/To-do-app/precache-manifest.c950064c22bdbb922d1398cdb599e085.js"
 );
 
 self.addEventListener('message', (event) => {
